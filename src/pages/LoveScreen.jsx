384225e06b549e49fb36c5fb2d6bb89daaeb0bb4
Sérgio Reis <142404631+serbiow@users.noreply.github.com>
2025-06-10 23:42:00 -0300

    Atualizar o LoveScreen.jsx

--- src/pages/LoveScreen.jsx
+++ src/pages/LoveScreen.jsx
@@ -10,7 +10,7 @@ const photos = [
   { url: '/photos/photo5.jpg', text: 'Eu te amo mais que tudo no universo, mais do que você consegue sequer imaginar 💗' },
   { url: '/photos/photo6.jpg', text: 'Espero poder compartilhar o resto da minha vida com você, eu te quero pra sempre 🫂' },
   { url: '/photos/photo7.PNG', text: 'E continuarei te querendo, mesmo que em outros universos ou outras vidas 🪞' },
-  { url: '/photos/photo8.jpg', text: 'Muito obrigado por tortar a minha vida melhor, por me amar e por ser você 😙' },
+  { url: '/photos/photo8.jpg', text: 'Muito obrigado por tornar a minha vida melhor, por me amar e por ser você 😙' },
 ];
 
 const LoveScreen = () => {
